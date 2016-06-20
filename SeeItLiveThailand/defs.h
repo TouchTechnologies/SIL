b69324bda4aa38adbde08610a68bc67920811a1f
batmanpon <weeraponssuwanchatree@weeraponss-MacBook-Air.local>
2016-06-20 10:10:38 +0700

    config 203

--- SeeItLiveThailand/defs.h
+++ SeeItLiveThailand/defs.h
@@ -1,4 +1,4 @@
-//ทดสอบ 1111111111111222222222333333333444444444
+
 //#define RoiApiURL @"http://office.touch-ics.com:81/framework/api/rois"
 #define RoiApiURL @"http://www.seeitlivethailand.com/api/rois"
 #define filter_limit 30
@@ -7,27 +7,26 @@
 //Production
 //#define service @"http://www.seeitlivethailand.com/"
 //#define PromotionURL @"http://www.seeitlivethailand.com"
-////#define StreamingHistoryURL @"http://www.seeitlivethailand.com/api/stream/histories?filtersPage=1&filterLimit=30"
 //#define StreamingHistoryURL @"http://www.seeitlivethailand.com/api/stream/histories"
 //#define StreamingLiveURL @"http://www.seeitlivethailand.com/api/liveStreamChannels?hasStreaming=Y"
 //#define SocketURL @"http://seeitlivethailand.com:3008"
 //#define StreamingHistoryURLByCatgory @"http://www.seeitlivethailand.com/api/liveStreamHistories"
 
 ///////////////Dev///////////////////
-#define service @"http://192.168.9.117/seeitlivethailand/"
-#define PromotionURL @"http://192.168.9.117/seeitlivethailand"
-//#define StreamingHistoryURL @"http://192.168.9.117/seeitlivethailand/api/stream/histories?filtersPage=1&filterLimit=30"
-#define StreamingHistoryURL @"http://192.168.9.117/seeitlivethailand/api/stream/histories"
-#define StreamingLiveURL @"http://192.168.9.117/seeitlivethailand/api/liveStreamChannels?hasStreaming=Y"
-#define SocketURL @"http://192.168.9.117:3008"
-#define StreamingHistoryURLByCatgory @"http://192.168.9.117/seeitlivethailand/api/liveStreamHistories"
+//#define service @"http://192.168.9.117/seeitlivethailand/"
+//#define PromotionURL @"http://192.168.9.117/seeitlivethailand"
+//#define StreamingHistoryURL @"http://192.168.9.117/seeitlivethailand/api/stream/histories"
+//#define StreamingLiveURL @"http://192.168.9.117/seeitlivethailand/api/liveStreamChannels?hasStreaming=Y"
+//#define SocketURL @"http://192.168.9.117:3008"
+//#define StreamingHistoryURLByCatgory @"http://192.168.9.117/seeitlivethailand/api/liveStreamHistories"
 
 //Dev2
-//#define service @"http://203.151.189.172/seeitlivethailand/"
-//#define PromotionURL @"http://203.151.189.172/seeitlivethailand"
-//#define StreamingHistoryURL @"http://203.151.189.172/seeitlivethailand/api/liveStreamHistories"
-//#define StreamingLiveURL @"http://203.151.189.172/seeitlivethailand/api/liveStreamChannels?hasStreaming=Y"
-//#define SocketURL @"http://192.168.9.117:3008"
+#define service @"http://203.151.189.172/seeitlivethailand/"
+#define PromotionURL @"http://203.151.189.172/seeitlivethailand"
+#define StreamingHistoryURL @"http://203.151.189.172/seeitlivethailand/api/stream/histories"
+#define StreamingLiveURL @"http://203.151.189.172/seeitlivethailand/api/liveStreamChannels?hasStreaming=Y"
+#define SocketURL @"http://203.151.189.172:3008"
+#define StreamingHistoryURLByCatgory @"http://203.151.189.172/seeitlivethailand/api/liveStreamHistories"
 
 
 
